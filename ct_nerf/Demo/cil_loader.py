e17cfa91b41ad9d5c2e8ad0100a2d391720a5ed7
chLi <565353780@qq.com>
2023-10-15 03:37:55 -0500

    finish load and trans data

--- ct_nerf/Demo/cil_loader.py
+++ ct_nerf/Demo/cil_loader.py
@@ -4,5 +4,5 @@ def demo():
     txrm_file_path = '/home/chli/Dataset/ICT/usb/gruppe 4/gruppe 4_2014-03-20_1404_12/tomo-A/gruppe 4_tomo-A.txrm'
 
     cil_loader = CILLoader(txrm_file_path)
-    cil_loader.showGeometry()
+    cil_loader.test()
     return True
--- ct_nerf/Module/cil_loader.py
+++ ct_nerf/Module/cil_loader.py
@@ -13,6 +13,7 @@ class CILLoader(object):
     def __init__(self, txrm_file_path=None) -> None:
         self.txrm_file_path = None
         self.data = None
+        self.trans_data = None
 
         if txrm_file_path is not None:
             assert self.loadTXRMFile(txrm_file_path)
@@ -21,6 +22,8 @@ class CILLoader(object):
     def reset(self):
         self.txrm_file_path = None
         self.data = None
+        self.trans_data = None
+        return True
 
     def loadTXRMFile(self, txrm_file_path):
         if not os.path.exists(txrm_file_path):
@@ -31,8 +34,48 @@ class CILLoader(object):
 
         self.txrm_file_path = txrm_file_path
         self.data = ZEISSDataReader(file_name=self.txrm_file_path).read()
+        self.data = TransmissionAbsorptionConverter()(self.data)
         return True
 
+    def getShape(self):
+        if self.data is None:
+            print('[WARN][CILLoader::getShape]')
+            print('\t data is None! please load data first')
+            return [0, 0, 0]
+
+        return self.data.shape
+
+    def getSliceNum(self):
+        return self.getShape()[0]
+
     def showGeometry(self):
+        if self.data is None:
+            print('[ERROR][CILLoader::showGeometry]')
+            print('\t data is None! please load data first')
+            return False
+
         show_geometry(self.data.geometry)
+        return True
+
+    def showData2D(self, slice_idx):
+        slice_num = self.getSliceNum()
+
+        if slice_idx < 0:
+            slice_idx += slice_num
+
+        if slice_idx < 0 or slice_idx >= slice_num:
+            print('[ERROR][CILLoader::showData2D]')
+            print('\t slice_idx out of range! valid range: [0, ' + \
+                str(slice_num - 1) + ']')
+            print('\t slice_idx:', slice_idx)
+            return False
+
+        show2D(self.data, slice_list=('angle', slice_idx))
+        return True
+
+    def test(self):
+        self.getShape()
+        self.showGeometry()
+        self.showData2D(0)
+        self.showData2D(-1)
         return True
